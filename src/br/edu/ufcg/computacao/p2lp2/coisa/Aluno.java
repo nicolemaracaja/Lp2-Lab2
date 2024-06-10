b0cc7ad38a0f6e8a23df2c35fe6f27a9fdfefeb8
nicol <nicol@DellNicole>
2024-06-10 11:48:28 -0300

    lab2

--- src/br/edu/ufcg/computacao/p2lp2/coisa/Aluno.java
+++ src/br/edu/ufcg/computacao/p2lp2/coisa/Aluno.java
@@ -4,16 +4,10 @@ public class Aluno {
 
 	private String nome;
 	private int anoNascimento;
-	private double cra;
 
 	public Aluno(String nome, int anoNascimento) {
 		this.nome = nome;
-		this.cra = 0.0;
 		this.anoNascimento = anoNascimento;
-	}
-
-	public void setCra(double cra) {
-		this.cra = cra;
 	}
 
 	public int getIdade() {
--- src/br/edu/ufcg/computacao/p2lp2/coisa/RegistroTempoOnline.java
+++ src/br/edu/ufcg/computacao/p2lp2/coisa/RegistroTempoOnline.java
@@ -47,7 +47,6 @@ public class RegistroTempoOnline {
 	*@param valor a quantidade de tempo registrada.
 	*/
 	public void adicionaTempoOnline (int valor) {
-		this.tempoOnline = tempoOnline;
 		if (valor > 0) {
 			tempoOnline += valor;
 		}
